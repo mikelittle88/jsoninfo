5eebb287f9c60d810ed1ca28fe288e790a876b17
Mike Little <cadelitt@gmail.com>
2019-05-02 16:55:35 -0400

    intial commit

---
+++ jsoninfo.py
@@ -0,0 +1,20 @@
+import json
+
+
+
+{
+    "firstName": "Michael",
+    "lastName": "Little",
+    "hobbies": ["snowboarding, skydiving, singing"],
+    "age": 21,
+    "children":[
+        {
+            "firstName": "Cade",
+            "age": 8
+        },
+        {
+            "firstName": "Atreyu",
+            "age": 16
+        }
+    ]
+}
